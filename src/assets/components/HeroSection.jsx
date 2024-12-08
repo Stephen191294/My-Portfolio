bc6bc8f0ac9b1109ab0709b0cc5ada5b352f26fc
Stephen191294 <150807027+Stephen191294@users.noreply.github.com>
2024-12-08 18:50:57 -0300

    update

--- src/assets/components/HeroSection.jsx
+++ src/assets/components/HeroSection.jsx
@@ -1,32 +1,39 @@
 import avatar from "../images/Group1(3).png";
 
-
-
 export default function HeroSection() {
   return (
     <div
       id="About"
-      className="flex flex-col md:flex-col lg:flex-row-reverse justify-center items-center h-screen wow animate__animated animate__fadeInLeft  wow"
+      className="flex flex-col pt-40  md:flex-col lg:flex-col justify-center items-center  wow animate__animated animate__fadeInLeft  wow"
     >
-      <div className="w-60 transition-transform duration-300 hover:animate-pulse cursor-pointer ml-10">
-      <img src={avatar} alt="avatar"/>
-      </div>
-      <div className="pt-10 flex flex-col items-center justify-center text-center">
-        <h1 className="dark:text-gray-100 text-xl typewriter sm:text-4xl md:text-4xl">
-          Hello, I'm <span className="text-green-light">Stephen Sullivan</span>
-          <span className="text-gray-400 typewriter sm:text-2xl block">
-            Front-end developer
-          </span>
-        </h1>
+      <div className="flex flex-col justify-center items-center lg:flex-row-reverse">
+        <div className="w-60 transition-transform duration-300 hover:animate-pulse cursor-pointer ml-10">
+          <img src={avatar} alt="avatar" />
+        </div>
+        <div className="pt-10 flex flex-col items-center justify-center text-center">
+          <h1 className="dark:text-gray-100 text-xl typewriter sm:text-4xl md:text-4xl">
+            Hello, I'm{" "}
+            <span className="text-green-light">Stephen Sullivan</span>
+            <span className="text-gray-400 typewriter sm:text-2xl block">
+              Front-end developer
+            </span>
+          </h1>
 
-        <div className="flex flex-col sm:flex-row gap-3 mt-5 items-center">
-          <button className="btn btn-wide bg-gradient-to-l from-green-light3 via-green-light2 to-green-light text-gray-100 border-none hover:from-green-600 hover:via-green-light3 hover:to-green-light2 transition-transform duration-300 hover:animate-pulse">
-            Download CV
-          </button>
+          <div className="flex flex-col sm:flex-row gap-3 mt-5 items-center">
+            <button
+              className="btn btn-wide bg-gradient-to-l from-green-light3 via-green-light2 to-green-light text-gray-100 border-none hover:from-green-600 hover:via-green-light3 hover:to-green-light2 transition-transform duration-300 hover:animate-pulse"
+              aria-label="Download CV"
+            >
+              Download CV
+            </button>
 
-          <button className="btn btn-wide bg-dark-900 text-white border-green-light transition-transform duration-300 hover:animate-pulse">
-            Contact Me
-          </button>
+            <button
+              className="btn btn-wide bg-dark-900 text-white border-green-light transition-transform duration-300 hover:animate-pulse"
+              aria-label="Contact Me"
+            >
+              Contact Me
+            </button>
+          </div>
         </div>
       </div>
     </div>
