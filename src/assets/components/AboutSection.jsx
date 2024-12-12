3c5f31504f6c6bf24eb71345fd8fc7b3bf48a65b
Stephen191294 <150807027+Stephen191294@users.noreply.github.com>
2024-12-12 08:42:44 -0300

    update

--- src/assets/components/AboutSection.jsx
+++ src/assets/components/AboutSection.jsx
@@ -3,11 +3,17 @@ export default function AboutSection() {
     <section className="wow animate__animated animate__fadeInLeft  wow">
       <h2 className="text-4xl text-center pt-5">About Me</h2>
       <p className="text-center sm:text-2xl sm:mb-10 p-5 animate__animated animate__fadeIn">
-        I'm a passionate, self-proclaimed designer who specializes in front-end
-        development (React.js, Bootstrap, Tailwind). I am very enthusiastic about bringing
-        the technical and visual aspects of digital products to life. User
-        experience, pixel-perfect design, and writing clear, readable, highly
-        performant code matters to me.
+        As a passionate and dedicated front-end developer, I bring to the table
+        a wealth of experience and a deep understanding of web development
+        technologies. My skill set includes proficiency in React, JavaScript,
+        HTML, CSS, Git, Sass, Figma, as well as frameworks such as Tailwind allowing me to create visually stunning
+        and highly functional user interfaces. I thrive in collaborative
+        environments, seamlessly integrating with teams to deliver projects on
+        time and with exceptional quality. My approach is centered around
+        problem-solving, continuous learning, and an unwavering commitment to
+        the craft of web development. I am always eager to innovate and
+        contribute to meaningful projects that push the boundaries of what the
+        web can do.
       </p>
     </section>
   );
--- src/assets/components/SkillsSection.jsx
+++ src/assets/components/SkillsSection.jsx
@@ -1,32 +1,29 @@
 import React from "react";
 import SkillCard from "./skillcard";
 
-
-
-
-
-
 export default function SkillsSection() {
   const skills = [
-    { iconClass: "fa-brands fa-js fa-4x", skillName: "JavaScript" },
-    { iconClass: "fa-brands fa-react fa-4x", skillName: "React" },
-    { iconClass: "fa-brands fa-sass fa-4x", skillName: "SASS" },
-    { iconClass: "fa-brands fa-git-alt fa-4x", skillName: "Git" },
-    { iconClass: "fa-brands fa-figma fa-4x", skillName: "Figma" },
-    { iconClass: "fa-brands fa-bootstrap fa-4x", skillName: "Bootstrap" },
-    { iconClass: "fa-brands fa-html5 fa-4x", skillName: "HTML 5" },
-    { iconClass: "fa-brands fa-css3-alt fa-4x", skillName: "CSS3" },
-    { iconClass: "fab fa-css3 fa-4x", skillName: "Tailwind" },
-
-    
+    { iconClass: "fa-brands fa-js fa-6x", skillName: "JavaScript" },
+    { iconClass: "fa-brands fa-react fa-6x", skillName: "React" },
+    { iconClass: "fa-brands fa-sass fa-6x", skillName: "SASS" },
+    { iconClass: "fa-brands fa-git-alt fa-6x", skillName: "Git" },
+    { iconClass: "fa-brands fa-figma fa-6x", skillName: "Figma" },
+    { iconClass: "fa-brands fa-bootstrap fa-6x", skillName: "Bootstrap" },
+    { iconClass: "fa-brands fa-html5 fa-6x", skillName: "HTML 5" },
+    { iconClass: "fa-brands fa-css3-alt fa-6x", skillName: "CSS3" },
   ];
 
   return (
-    <section id="Skills" className="h-screen pt-32 flex flex-col justify-center items-center  wow animate__animated animate__fadeInLeft">
+    <section
+      id="Skills"
+      className="h-screen pt-32 flex flex-col justify-center items-center  wow animate__animated animate__fadeInLeft"
+    >
       <h3 className="text-center text-2xl sm:text-4xl pb-5">Skills</h3>
-      <p className="text-center sm:text-2xl">Technologies that I'm really good at</p>
+      <p className="text-center sm:text-2xl">
+        Technologies that I'm really good at
+      </p>
       <div className="container mx-auto flex items-center justify-center mt-10 wow animate__animated animate__fadeInLeft  wow ">
-        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-4">
           {skills.map((skill, index) => (
             <SkillCard key={index} {...skill} />
           ))}
