13f8f3778be85081d33f71b6d8cbc94fb54622c2
Stephen191294 <150807027+Stephen191294@users.noreply.github.com>
2024-12-23 20:07:18 -0300

    update

--- src/assets/components/HeroSection.jsx
+++ src/assets/components/HeroSection.jsx
@@ -8,25 +8,25 @@ export default function HeroSection() {
     >
       <div className="flex flex-col justify-center items-center lg:flex-row-reverse">
         <div className="w-80 transition-transform duration-300  cursor-pointer ml-10 hover:animate-pulse">
-          <img  src={avatar} alt="avatar"/>
+          <img src={avatar} alt="avatar" />
         </div>
         <div className="pt-10 flex flex-col items-center justify-center text-center">
           <h1 className="dark:text-gray-100 text-xl typewriter sm:text-4xl md:text-4xl">
-            Hello, I'm{" "}
-            <span className=" hero-span ">Stephen Sullivan</span>
+            Hello, I'm <span className=" hero-span ">Stephen Sullivan</span>
             <span className="text-gray-400 typewriter sm:text-2xl block">
               Front-end developer
             </span>
           </h1>
 
           <div className="flex flex-col sm:flex-row gap-3 mt-5 items-center">
-            <button
+            <a
               className="btn btn-wide bg-dark-900 text-white border-green-light transition-transform duration-300 hover:animate-pulse hover:text-green-light"
               aria-label="Download CV"
-              href
+              href="/resume.pdf"
+              download="resume.pdf"
             >
               Download CV
-            </button>
+            </a>
 
             <button
               className="btn btn-wide bg-dark-900 text-white border-green-light transition-transform duration-300 hover:animate-pulse hover:text-green-light"
